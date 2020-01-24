e18c379bbf93bcf35cfc64a3b7191a8babfba471
marllonramos <87marllon@gmail.com>
2020-01-24 16:34:24 -0300

    implement remove and update

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ListPlayer from '../components/ListPlayer';
 import Login from '../components/Login';
+import CrudPlayer from '../components/CrudPlayer';
 
 const Routes = () => {
     return(
@@ -9,6 +10,7 @@ const Routes = () => {
             <Switch>
                 <Route exact path="/" component={ListPlayer} />
                 <Route exact path="/loginis" component={Login} />
+                <Route exact path="/crud" component={CrudPlayer} />
             </Switch>
         </BrowserRouter>
     );
